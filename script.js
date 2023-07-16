b8f4faab6c2ba73fb6ba727e3263d64cfc254ac2
Bethany Joyce Sibala <bethanyjoycesibala@gmail.com>
2023-07-16 19:09:59 +0800

    what's wrong with this code

--- script.js
+++ script.js
@@ -4,10 +4,18 @@
 
 // calculate a senior discount
 // variables: name, bill, pax, seniorPax, discount;
+function calculate(){
+    const fullName = document.getElementById("fullName").value;
+    const numberOfPax = document.getElementById("pax").value;
+    const numberOfSenior = document.getElementById("seniorPax").value;
 
-const calculateBill = (bill, pax, seniorPax) => {
-    const totalBill = bill - (((bill / pax) * seniorPax) * 0.20);
-    return totalBill;
+    const calculateBill = (bill, pax, seniorPax) => {
+        const totalBill = bill - (((bill / pax) * seniorPax) * 0.20);
+        return totalBill;
+    }
+    console.log(calculateBill(fullName, numberOfPax, numberOfSenior));
 }
 
-// console.log(calculateBill(1000, 10, 1));
+
+
+
